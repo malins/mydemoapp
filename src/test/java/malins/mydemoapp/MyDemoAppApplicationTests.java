6dc620e7b3e2ecfb8e0e9f2a598308e9e645f9e6
manuell <manuell-android@yandex.com>
2017-06-07 14:17:06 +0200

    Initial commit.

---
+++ src/test/java/malins/mydemoapp/MyDemoAppApplicationTests.java
@@ -0,0 +1,16 @@
+package malins.mydemoapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyDemoAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
